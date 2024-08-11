0a717d1b81b1f785ddfd7c106ce2f1354358f255
sdetm <sdetm@HP>
2024-08-11 12:16:53 +0530

    fourth commit

--- Git_Prac/src/GitPrac/Github.java
+++ Git_Prac/src/GitPrac/Github.java
@@ -1,14 +1,17 @@
 package GitPrac;
 
 public class Github {
+	int a=5;
 
 	public static void main(String[] args) {
+		Github ob=new Github();	
+		int b=ob.a;
 		System.out.println("Welcome to Git Hub");
 		System.out.println("Welcome to Java");
 		System.out.println("Welcome to Git Hub");
 		System.out.println("Welcome to Git Hub");
 		System.out.println("Welcome to Git Hub");
-
+        System.out.println(b);
 	}
 
 }
