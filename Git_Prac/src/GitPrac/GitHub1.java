a532b9e74062a19d64e06c3744cf1389d85e32e7
sdetm <sdetm@HP>
2024-08-11 12:12:11 +0530

    Third commit

--- Git_Prac/src/GitPrac/GitHub1.java
+++ Git_Prac/src/GitPrac/GitHub1.java
@@ -3,8 +3,7 @@ package GitPrac;
 public class GitHub1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("code commited to standarize reposit");
 	}
 
 }
