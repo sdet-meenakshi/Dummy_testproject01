646cbea4851d7578f7629627ccf7cd8a0956d1e0
sdetm <sdetm@HP>
2024-08-11 12:03:12 +0530

    First time commit

---
+++ Git_Prac/src/GitPrac/Github.java
@@ -0,0 +1,14 @@
+package GitPrac;
+
+public class Github {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Git Hub");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Git Hub");
+		System.out.println("Welcome to Git Hub");
+		System.out.println("Welcome to Git Hub");
+
+	}
+
+}
